75e7cd34643805f4d4e019b58cfb0ba7e7f85dd8
Misha Wagner <mishajw@gmail.com>
2019-04-22 12:45:54 +0100

    Initial commit

---
+++ informational_influence/__main__.py
@@ -0,0 +1,10 @@
+# pylint: disable=missing-docstring
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser("informational_influence")
+    _ = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
